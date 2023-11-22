cb667e8295c21543eefad04ca059d439ef220630
andriby <adrian.dryzara3@gmail.com>
2023-11-22 14:29:29 -0500

    depositar.component avances

--- src/app/pages/servicios/staking/staking.component.ts
+++ src/app/pages/servicios/staking/staking.component.ts
@@ -6,16 +6,6 @@ import { ApiService, IAPICore } from 'src/app/services/apicore/api.service';
 import { MatDialog } from '@angular/material/dialog';
 import { PagoComponent } from '../../trade/pago/pago.component';
 
-export interface stakingData{
-  id: string,
-  id_staking: number,
-  capital: number,
-  porcentaje_ganancia: number,
-  fecha_pago: string,
-  dias_restantes: number,
-  recibo: number
-}
-
 @Component({
   selector: 'app-staking',
   templateUrl: './staking.component.html',
@@ -23,54 +13,17 @@ export interface stakingData{
 })
 export class StakingComponent implements OnInit {
 
-  // public xApi: IAPICore = {
-  //   funcion : '',
-  //   parametros: ''
-  // }
-
-  //public ELEMENT_DATA : Maestro[] = []
-
-  displayedColumns: string[] = ['id_staking', 'capital', 'porcentaje_ganancia', 'fecha_pago', 'dias_restantes', 'recibo'];
-  dataSource = MatTableDataSource<stakingData>;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  //@Input('columnas') displayedColums: string[];
-
-  //@Input () API: any = ['codigo', 'nombre', 'descripcion'];
-
   screen:any = {width: 0}
 
   constructor(public dialog:
      MatDialog, private apiService: ApiService) {
-      const data = Array.from({length: 100}, (_, k) => createNew(k + 1));
 
-      this.dataSource = new MatTableDataSource(data);
    }
 
   async ngOnInit(): Promise<void> {
     this.screen.width = window.innerWidth;
     window.addEventListener('resize', this.onResize.bind(this))
-
-    // console.log('Entrando a la conexion ', this.API)
-    // if (this.API != "0") {
-    //   await this.cargarContenido()
-    // }
   }
-
-  // cargarContenido(): void{
-  //   this.ELEMENT_DATA = []
-  //   this.xApi.funcion = "CCEC_CCONTENIDO"
-  //   this.xApi.parametros = this.API
-  //   this.apiService.Ejecutar(this.xApi).subscribe(
-  //     (data) => {
-  //       this.ELEMENT_DATA = data.Cuerpo
-  //       this.dataSource = new MatTableDataSource<Maestro>(this.ELEMENT_DATA)
-  //       this.dataSource.paginator = this.paginator
-
-  //     }
-  //   )
-  // }
 
   onResize(event: Event): void{
     this.screen.width = (event.target as Window).innerWidth
@@ -86,24 +39,4 @@ export class StakingComponent implements OnInit {
     })
   }
 
-}
-
-function createNew(id:number): stakingData {
-
-  const id_staking = 0
-  const capital = 0
-  const porcentaje_ganancia = 0
-  const fecha_pago = ""
-  const dias_restantes = 0
-  const recibo = 0
-
-  return {
-    id_staking: id_staking,
-    capital: capital,
-    porcentaje_ganancia: porcentaje_ganancia,
-    fecha_pago: fecha_pago,
-    dias_restantes: dias_restantes,
-    recibo: recibo,
-    id: id.toString()
-  }
 }
--- src/app/pages/trade/pago/pago.component.ts
+++ src/app/pages/trade/pago/pago.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
+interface Servicio {
+  value: string;
+  viewValue: string;
+}
+
+interface Cuenta {
+  value: string;
+  viewValue: string;
+}
+
+interface Metodo {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-pago',
   templateUrl: './pago.component.html',
-  styleUrls: ['./pago.component.css']
+  styleUrls: ['./pago.component.scss']
 })
 export class PagoComponent implements OnInit {
 
@@ -12,5 +27,28 @@ export class PagoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  servicios: Servicio[] = [
+    {value: 'staking-0', viewValue: 'Staking'},
+    {value: 'criptoBolso-1', viewValue: 'Cripto Bolso'},
+    {value: 'grid-2', viewValue: 'Grid'},
+  ];
+
+  cuentas: Cuenta[] = [
+    {value: 'cuenta-0', viewValue: 'Cuenta 1'},
+    {value: 'cuenta-1', viewValue: 'Cuenta 2'},
+    {value: 'cuenta-2', viewValue: 'Cuenta 3'},
+  ];
+
+  metodos: Metodo[] = [
+    {value: 'metodo-0', viewValue: 'Metodo 1'},
+    {value: 'metodo-1', viewValue: 'Metodo 2'},
+    {value: 'metodo-2', viewValue: 'Metodo 3'},
+  ];
+
 
 }
